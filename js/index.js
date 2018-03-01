721827a4e4e5c2590c5ffa45627093a376cb6302
Alexey Kuchyn <forrana@gmail.com>
2018-03-01 23:57:20 +0200

    Listen to update messages from service worker

--- js/index.js
+++ js/index.js
@@ -7,8 +7,27 @@ MobileDragDrop.polyfill({
 
 document.addEventListener('DOMContentLoaded', () => draggability(), false);
 
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker
-//            .register('./service-worker.js')
-//            .then(function() { console.log('Service Worker Registered'); });
-// }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+           .register('./service-worker.js')
+           .then(function() {
+             console.log('Service Worker Registered');
+           });
+  navigator.serviceWorker.onmessage = function (evt) {
+    var message = JSON.parse(evt.data);
+
+    var isRefresh = message.type === 'refresh';
+    var isAsset = message.url.includes('asset');
+    var lastETag = localStorage.currentETag;
+
+    var isNew =  lastETag !== message.eTag;
+    if (isRefresh && isAsset && isNew) {
+      if (lastETag) {
+        notice.innerHTML = "New version available. Please reload page.";
+      }
+      localStorage.currentETag = message.eTag;
+    }
+  }
+
+  var notice = document.querySelector('#update-notice');
+}
