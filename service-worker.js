d3789a31f77c8e55f25278dd468e3e0a43b1ea3e
Alexey Kuchyn <forrana@gmail.com>
2018-03-01 23:56:16 +0200

    service worker changes * Install new version immediatly * Send message to client if there is a new cashe available

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,33 @@
-// Copyright 2016 Google Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 var dataCacheName = 'GrndVsInf-v1';
 var cacheName = 'GrndVsInf-final-1';
+var version = '1.0.0';
+const CACHE = 'GrndVsInf-final-1';
 var filesToCache = [
-  'index.html',
-  'js/alarm.js',
-  'js/draggability.js',
-  'js/health.js',
-  'js/index.js',
-  'css/main.css',
-  'css/normalize.css',
-  'libs/mobile-drag-drop/default.css',
-  'libs/mobile-drag-drop/scroll-behaviour.min.js',
-  'libs/compromise/builds/compromise.es6.min.js'
+  '/index.html',
+  '/js/alarm.js',
+  '/js/app.js',
+  '/js/constants.js',
+  '/js/draggability.js',
+  '/js/guessed.js',
+  '/js/health.js',
+  '/js/leveling-system.js',
+  '/js/wordsmanager.js',
+  '/css/main.css',
+  '/css/normalize.css',
+  '/libs/mobile-drag-drop/default.css',
+  '/libs/mobile-drag-drop/scroll-behaviour.min.js',
+  '/libs/compromise/builds/compromise.es6.min.js'
 ];
 
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
+self.addEventListener('install', function(evt) {
+  console.log('[ServiceWorker] Installed version', version);
+  evt.waitUntil(
+    precache()
+    .then(function() {
+      console.log('[ServiceWorker] Skip waiting on install');
+      return self.skipWaiting();
     })
-  );
+  )
 });
 
 self.addEventListener('activate', function(e) {
@@ -49,48 +42,53 @@ self.addEventListener('activate', function(e) {
       }));
     })
   );
-  /*
-   * Fixes a corner case in which the app wasn't returning the latest data.
-   * You can reproduce the corner case by commenting out the line below and
-   * then doing the following steps: 1) load app for first time so that the
-   * initial New York City data is shown 2) press the refresh button on the
-   * app 3) go offline 4) reload the app. You expect to see the newer NYC
-   * data, but you actually see the initial data. This happens because the
-   * service worker is not yet activated. The code below essentially lets
-   * you activate the service worker faster.
-   */
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(e) {
-  console.log('[Service Worker] Fetch', e.request.url);
-  var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
-  if (e.request.url.indexOf(dataUrl) > -1) {
-    /*
-     * When the request URL contains dataUrl, the app is asking for fresh
-     * weather data. In this case, the service worker always goes to the
-     * network and then caches the response. This is called the "Cache then
-     * network" strategy:
-     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
-     */
-    e.respondWith(
-      caches.open(dataCacheName).then(function(cache) {
-        return fetch(e.request).then(function(response){
-          cache.put(e.request.url, response.clone());
-          return response;
-        });
-      })
-    );
-  } else {
-    /*
-     * The app is asking for app shell files. In this scenario the app uses the
-     * "Cache, falling back to the network" offline strategy:
-     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
-     */
-    e.respondWith(
-      caches.match(e.request).then(function(response) {
-        return response || fetch(e.request);
-      })
-    );
-  }
+self.addEventListener('fetch', function(evt) {
+  console.log('[Service Worker] Fetch', evt.request.url);
+  if( evt.request.url.includes('chrome-extension:')
+      || evt.request.url.includes('socket.io')
+      || evt.request.url.includes('google-analytic')
+    ) return;
+  evt.respondWith(fromCache(evt.request));
+  evt.waitUntil(update(evt.request).then(refresh));
 });
+
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    console.log('[ServiceWorker] Caching app shell');
+    return cache.addAll(filesToCache);
+  });
+}
+
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+function update(request) {
+  return caches.open(CACHE).then(function (cache) {
+   return fetch(request).then(function (response) {
+     return cache.put(request, response.clone()).then(function () {
+       return response;
+     });
+   });
+  });
+}
+
+function refresh(response) {
+  return self.clients.matchAll().then(function (clients) {
+    clients.forEach(function (client) {
+      var message = {
+        type: 'refresh',
+        url: response.url,
+        eTag: response.headers.get('ETag')
+      }
+      client.postMessage(JSON.stringify(message));
+    })
+  })
+}
